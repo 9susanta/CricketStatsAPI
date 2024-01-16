d636cc7dafaf3520560b389e7cb5fe8859bf8a7b
9susanta <rsusanta0@gmail.com>
2024-01-16 23:15:19 +0530

    Interceptor Implemented

--- NgRx/src/app/auth/state/auth.selector.ts
+++ NgRx/src/app/auth/state/auth.selector.ts
@@ -7,3 +7,6 @@ const getAuthState = createFeatureSelector<AuthState>(AUTH_STATE);
 export const isAuthenticatedSelector = createSelector(getAuthState, (state) => {
   return state.user ? true : false;
 });
+export const getToken = createSelector(getAuthState, (state) => {
+  return state.user ? state.user.userToken : null;
+});
