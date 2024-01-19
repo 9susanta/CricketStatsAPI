5f9d7563bced4d81aa4e7370e798726194fdd2fb
9susanta <rsusanta0@gmail.com>
2024-01-19 23:57:37 +0530

    Student Model Added

---
+++ CricketStatsAPI/Model/Student.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace CricketStatsAPI.Model
+{
+    public class Student
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Mark { get; set; }
+    }
+}
