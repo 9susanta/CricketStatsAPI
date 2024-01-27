f572751fcf78f84f4f3287d6b26bad7c23673e2d
9susanta <rsusanta0@gmail.com>
2024-01-27 23:35:32 +0530

    Implemented Entity Adpter

--- NgRx/src/app/student/state/student.state.ts
+++ NgRx/src/app/student/state/student.state.ts
@@ -1,9 +1,11 @@
+import { EntityState, createEntityAdapter } from '@ngrx/entity';
 import { Student } from 'src/app/models/student.model';
 
-export interface StudentState {
-  students: Student[];
-}
+export interface StudentState extends EntityState<Student> {}
+export const studentAdapter = createEntityAdapter<Student>();
 
-export const initialState: StudentState = {
-  students: [],
-};
+export const initialState: StudentState = studentAdapter.getInitialState();
+
+
+
+
